b2bb5fd9292ff4edde17f25d8e528d40fbe0573f
naiaraf-dev <naiaraf-dev@gmail.com>
2025-03-18 21:25:27 -0300

    clase Pokemon

--- claseGit_18_03/Program.cs
+++ claseGit_18_03/Program.cs
@@ -7,4 +7,24 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+    public class Pokemon
+    {
+        //Atributos
+        public string nombre;
+        public int salud;
+        public string raza;
+
+
+        //Metodos
+        public void Atacar()
+        {
+            Console.WriteLine("Ataco!");
+        }
+
+        public void Defender()
+        {
+            Console.WriteLine("Activo defensa");
+        }
+    }
 }
